32b22bf3f200ca1137728837bc0e951c2560f33c
Naveed Arif <156594573+naveedarif97x@users.noreply.github.com>
2024-02-18 20:27:00 +0500

    Delete Alerts.cy.js

--- Alerts.cy.js
+++
@@ -1,8 +0,0 @@
-/// <reference types="Cypress" />
-
-describe("Alerts", ()=>{
-    it("Simple Alert", ()=>{
-        cy.visit("https://demoqa.com/alerts")
-        cy.get("#alertButton").click()
-    })
-})
